3af337421cbf7a8b5962e8d8d3c41491a421457c
verdes ben <verdesb.devacc@gmail.com>
2024-11-10 12:54:02 +0100

    fix layout

--- app/components/ui/sparkles.tsx
+++ app/components/ui/sparkles.tsx
@@ -80,7 +80,7 @@ export const SparklesCore = (props: ParticlesProps) => {
                   enable: false,
                   mode: "repulse",
                 },
-                // @ts-ignore
+                // @typescript-eslint/no-explicit-any
                 resize: true as any,
               },
               modes: {
