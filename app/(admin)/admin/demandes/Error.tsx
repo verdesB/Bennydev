89cdd2b16994b5cb2b3a705b3fc8efa8e8a55d75
verdes ben <verdesb.devacc@gmail.com>
2024-12-06 20:01:46 +0100

    fix bug route files md/image

--- app/(admin)/admin/demandes/Error.tsx
+++ app/(admin)/admin/demandes/Error.tsx
@@ -1,4 +1,4 @@
-const Error = ({ error }) => (
+const Error = ({ error }: { error: string }) => (
     <div className="flex items-center justify-center min-h-[400px]">
         <div className="text-destructive bg-destructive/10 p-4 rounded-lg">
             <p className="font-semibold">Erreur: {error}</p>
