59d8de806dd7d00b6e8c643e38c041d096c9843d
verdes ben <verdesb.devacc@gmail.com>
2024-12-06 18:46:50 +0100

    fix bug route files md/image

--- app/(admin)/admin/demandes/[id]/page.tsx
+++ app/(admin)/admin/demandes/[id]/page.tsx
@@ -5,12 +5,6 @@ import { Metadata } from 'next'
 
 
 
-export default function Page({ params }: { params: { id: string } }) {
-    return <p>Demande {params.id}</p>
-}
-
-export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
-    return {
-        title: `Demande ${params.id}`,
-    }
+export default function Page() {
+    return <p>Demande </p>
 }
