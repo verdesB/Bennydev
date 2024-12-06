1d9b8839fa38ff692d19dd006d060ebb968e96dc
verdes ben <verdesb.devacc@gmail.com>
2024-12-06 17:15:24 +0100

    fix bug route files md/image

--- app/(admin)/admin/demande/[id]/page.tsx
+++ app/(admin)/admin/demande/[id]/page.tsx
@@ -3,9 +3,10 @@ import DemandeDetailClient from './DemandeDetailClient'
 type Props = {
     params: {
         id: string
-    }
+    },
+    searchParams: { [key: string]: string | string[] | undefined }
 }
 
-export default function Page(props: Props) {
-    return <DemandeDetailClient {...props} />
+export default async function Page({ params }: Props) {
+    return <DemandeDetailClient params={params} />
 } 
