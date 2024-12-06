6431766c65df8b793b27ea7c3ede8f0035a37571
verdes ben <verdesb.devacc@gmail.com>
2024-12-06 23:40:05 +0100

    fix bug route files md/image

--- app/(client)/client/help/page.tsx
+++ app/(client)/client/help/page.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-import { useEffect, useState, Suspense } from 'react'
+import { useState, Suspense } from 'react'
 import { Separator } from '@/components/ui/separator'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { cn } from '@/lib/utils'
-import { useRouter, useSearchParams } from 'next/navigation'
+import { useRouter} from 'next/navigation'
 import { Input } from '@/components/ui/input'
 import { Search } from 'lucide-react'
 import { sections } from './section.data'
