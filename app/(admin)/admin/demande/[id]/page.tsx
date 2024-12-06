e988f50121c3aded10495410229951a33fc7fa63
verdes ben <verdesb.devacc@gmail.com>
2024-12-06 17:31:33 +0100

    fix bug route files md/image

--- app/(admin)/admin/demande/[id]/page.tsx
+++ app/(admin)/admin/demande/[id]/page.tsx
@@ -1,9 +1,13 @@
-import DemandeDetailClient from "./DemandeDetailClient"
 
-export default async function Page({ 
-    params,
-}: {
+import { NextPage } from 'next'
+import DemandeDetailClient from './DemandeDetailClient'
+
+type PageProps = {
     params: { id: string }
-}) {
+}
+
+const Page: NextPage<PageProps> = async ({ params }) => {
     return <DemandeDetailClient params={params} />
 }
+
+export default Page
