2092bb4caa0cc271ee1e1a9b2b3a6754f9daba5c
verdes ben <verdesb.devacc@gmail.com>
2024-12-06 19:05:00 +0100

    fix bug route files md/image

--- app/(admin)/admin/demandes/[id]/page.tsx
+++ app/(admin)/admin/demandes/[id]/page.tsx
@@ -1,4 +1,4 @@
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
     const resolvedParams = await params;
-    return <p>Demande pour l'ID {resolvedParams.id}</p>;
+    return <p>Demande pour l&apos;ID {resolvedParams.id}</p>;
 }
