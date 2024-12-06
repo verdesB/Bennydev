c67a481072be751e83b93182491ae581aeaffef5
verdes ben <verdesb.devacc@gmail.com>
2024-12-06 18:38:18 +0100

    fix bug route files md/image

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -49,7 +49,7 @@ export default function AdminLayout({
     },
     {
       title: "Demandes",
-      href: "/admin/demande",
+      href: "/admin/demandes",
       icon: <FileText className="h-4 w-4" />,
     },
     {
